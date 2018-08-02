236a16e9a2854287f46ad71d7b8ff925a94fbb7e
Samuel pires <samuel.pires.pro@gmail.com>
2018-08-02 18:18:39 +0100

    shit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+user_serialized = open(url).read
+user = JSON.parse(user_serialized)
+
+
+
+user["drinks"].each do |cocktail|
+ Ingredient.create(name: cocktail["strIngredient1"])
+end
